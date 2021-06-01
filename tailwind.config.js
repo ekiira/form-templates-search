e5dc7f794a60148fa8c3b6ae8b166456077777a2
Oluwatosin Jimoh <57687166+ekiira@users.noreply.github.com>
2021-06-01 23:59:51 +0100

    setup global colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,40 @@
+const { white } = require("tailwindcss/colors");
+
 module.exports = {
   purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
+    colors: {
+      white: white,
+      green: {
+        light: "var(--lightGreen)",
+      },
+      orange: {
+        DEFAULT: "var(--orange)",
+      },
+      deepBlue: {
+        DEFAULT: "var(--deepBlue)",
+      },
+      black: {
+        DEFAULT: "var(--black)",
+        dark: "var(--deep)",
+      },
+      neutral: {
+        DEFAULT: "var(--neutral)",
+      },
+      neutral7: {
+        DEFAULT: "var(--neutral-7)",
+      },
+      gray: {
+        light: "var(--lightGray)",
+        DEFAULT: "var(--gray)",
+        dark: "var(--deepGray)",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
